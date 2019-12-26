4bb888f3fc14b3eaf954a3c6a35d9a49d3767dd6
bzreinhardt <bzreinhardt@gmail.com>
2019-12-26 15:28:23 +0800

    working user queries

--- app/models.py
+++ app/models.py
@@ -4,18 +4,6 @@ from flask_login import UserMixin
 import datetime
 
 
-class Account(UserMixin, db.Model):
-
-    __tablename__ = 'accounts'
-
-    account_id = db.Column(db.Integer, primary_key=True)
-    firebase_user_id = db.Column(db.Text, unique=True)
-    email = db.Column(db.Text, unique=True, nullable=False)
-    email_verified = db.Column(db.Boolean, default=False, nullable=False)
-    name = db.Column(db.Text)
-    photo_url = db.Column(db.Text)
-    
-
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -25,8 +13,13 @@ class User(UserMixin, db.Model):
     registered_on = db.Column(db.DateTime, nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
     picture = db.Column(db.String(), nullable=True)
+    name = db.Column(db.String(), nullable=True)
 
-    def __init__(self, social_id, picture=None, name=None, email=None, admin=False):
+    def __init__(self, social_id,
+                picture=None,
+                name = None,
+                email = None,
+                admin=False):
         self.social_id = social_id
         self.picture = picture
         self.name = name
--- app/routes.py
+++ app/routes.py
@@ -5,29 +5,58 @@ from firebase_admin import credentials, auth
 import datetime
 import traceback
 import json
+import pdb
 
-def get_user(request):
+def get_user_from_token(token):
+    user = None
+    try:
+        decoded_token = auth.verify_id_token(token)
+        user = User.query.filter_by(social_id=decoded_token['user_id']).first()
+    except Exception:
+        # Session cookie is invalid, expired or revoked. Force user to login.
+        traceback.print_exc()
+    return user
+
+def get_user_from_request(request):
     session_cookie = request.cookies.get('session')
     user = None
     if session_cookie:
         try:
-            decoded_token = auth.verify_id_token(session_cookie)
-            user = {"name":decoded_token["name"]}
+            user = get_user_from_token(session_cookie)
         except auth.InvalidSessionCookieError:
             # Session cookie is invalid, expired or revoked. Force user to login.
             traceback.print_exc()
+    if user:
+        print(user.social_id)
     return user
+
+
 
 @app.route('/sessionLogin', methods=['POST'])
 def session_login():
     # Get the ID token sent by the client
     id_token = request.form['idToken']
+    provider_data = json.loads(request.form['providerData'])
+
     # Set session expiration to 5 days.
     expires_in = datetime.timedelta(days=5)
     try:
         # Create the session cookie. This will also verify the ID token in the process.
         # The session cookie will have the same claims as the ID token.
         session_cookie = auth.create_session_cookie(id_token, expires_in=expires_in)
+        user = get_user_from_token(id_token)
+        print(user)
+        decoded_token = auth.verify_id_token(id_token)
+        if not user:
+            user = User(decoded_token['user_id'])
+            db.session.add(user)
+        if 'picture' in decoded_token:
+            user.picture = decoded_token['picture']
+        if 'email' in provider_data:
+            user.email = provider_data['email']
+        if 'name' in decoded_token:
+            user.name = decoded_token['name']
+        db.session.commit()
         response = jsonify({'status': 'success'})
         expires = datetime.datetime.now() + expires_in
         response.set_cookie('session', id_token)
@@ -47,8 +76,10 @@ def session_logout():
 @app.route('/')
 @app.route('/index')
 def index():
-    user = get_user(request)
+    user = get_user_from_request(request)
     render_login = True
+    user_name = None
     if user:
         render_login = False
-    return render_template('index.html', user=user, render_login = json.dumps(render_login))
+        user_name = user.name
+    return render_template('index.html', user_name=user_name, render_login = json.dumps(render_login))
